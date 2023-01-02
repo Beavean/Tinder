8662624ee760517404592d8ce2c745d3158b27c4
Anthony Petrov <beavean@gmail.com>
2023-01-03 00:58:23 +0200

    add configuration to MatchView

--- Tinder/Views/MatchView.swift
+++ Tinder/Views/MatchView.swift
@@ -9,22 +9,144 @@ import UIKit
 
 class MatchView: UIView {
     
+    // MARK: - UI Elements
+    
+    private lazy var views = [
+        matchImageView,
+        descriptionLabel,
+        currentUserImageView,
+        matchedUserImageView,
+        sendMessageButton,
+        keepSwipingButton
+    ]
+    
+    private let matchImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(systemName: "exclamationmark.circle"))
+        imageView.contentMode = .scaleAspectFill
+        imageView.tintColor = .red
+        return imageView
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = .white
+        label.text = "You liked each other!"
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let currentUserImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.borderWidth = 2
+        imageView.layer.borderColor = UIColor.white.cgColor
+        return imageView
+    }()
+    
+    private let matchedUserImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.borderWidth = 2
+        imageView.layer.borderColor = UIColor.white.cgColor
+        return imageView
+    }()
+    
+    private lazy var sendMessageButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Send Message", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(didTapSendMessage), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var keepSwipingButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Send Message", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(didTapKeepSwiping), for: .touchUpInside)
+        return button
+    }()
+    
+    private let visualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
+    
     // MARK: - Properties
     
     private let currentUser: User
     private let matchedUser: User
-    
-    // MARK: - Init
+        
+    // MARK: - Lifecycle
     
     init(currentUser: User, matchedUser: User) {
         self.currentUser = currentUser
         self.matchedUser = matchedUser
         super.init(frame: .zero)
-        backgroundColor = .orange
+        configureBlurView()
+        configureUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Actions
+    
+    @objc private func didTapSendMessage() {
+        
+    }
+    
+    @objc private func didTapKeepSwiping() {
+        
+    }
+    
+    @objc private func handleDismissal() {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1) {
+            self.alpha = 0
+        } completion: { _ in
+            self.removeFromSuperview()
+        }
+    }
+    
+    // MARK: - Helpers
+    
+    private func configureUI() {
+        views.forEach { view in
+            addSubview(view)
+            view.alpha = 1
+        }
+        currentUserImageView.anchor(left: centerXAnchor, paddingLeft: 16)
+        currentUserImageView.setDimensions(height: 140, width: 140)
+        currentUserImageView.layer.cornerRadius = 140 / 2
+        currentUserImageView.centerY(inView: self)
+        
+        matchedUserImageView.anchor(right: centerXAnchor, paddingRight: 16)
+        matchedUserImageView.setDimensions(height: 140, width: 140)
+        matchedUserImageView.layer.cornerRadius = 140 / 2
+        matchedUserImageView.centerY(inView: self)
+        
+        sendMessageButton.anchor(top: currentUserImageView.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 32, paddingLeft: 48, paddingRight: 48)
+        
+        keepSwipingButton.anchor(top: sendMessageButton.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 16, paddingLeft: 48, paddingRight: 48)
+        
+        descriptionLabel.anchor(left: leftAnchor, bottom: currentUserImageView.topAnchor, right: rightAnchor, paddingBottom: 32)
+        
+        matchImageView.anchor(bottom: descriptionLabel.topAnchor)
+        matchImageView.setDimensions(height: 80, width: 300)
+        matchImageView.centerX(inView: self)
+        
+    }
+    
+    private func configureBlurView() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleDismissal))
+        visualEffectView.addGestureRecognizer(tap)
+        addSubview(visualEffectView)
+        visualEffectView.fillSuperview()
+        visualEffectView.alpha = 0
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.visualEffectView.alpha = 1
+        }, completion: nil)
+    }
 }
